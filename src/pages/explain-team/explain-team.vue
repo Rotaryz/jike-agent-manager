01ea105dcc455f2a34e99a31f838559407d9223e
wengtianliang <wengtianliang@jkweixin.com>
2018-08-15 15:50:06 +0800

    fix text

--- src/pages/explain-team/explain-team.vue
+++ src/pages/explain-team/explain-team.vue
@@ -4,7 +4,7 @@
     <div class="tem-box">
       <div class="echarts-title">
         <div class="line"></div>
-        <div class="text">收入统计说明</div>
+        <div class="text">宗旨</div>
       </div>
       <div class="big-small title-mt">代理商作为我们的合作伙伴，我们的宗旨是帮助我们的合作伙伴能赚钱，赚得到钱。</div>
       <div class="echarts-title box-mt">
@@ -21,7 +21,7 @@
         <div class="line"></div>
         <div class="text">我的收入分销</div>
       </div>
-      <div class="samll-title">团队成员升级正式版奖励：</div>
+      <div class="samll-title">代理商的分销收入有两条途径：</div>
       <div class="big-small mt-10">第一，团队成员升级正式版本奖励：代理商间接成员推荐的好友开通店铺之后，有30天的试用期。在试用期内，该好友可以自费升级成为正式版本的店铺，升级费用为298元/单。根据赞播分销奖励政策规定，该好友的直接推荐人奖励100元/单，代理商奖励90元/单。</div>
       <div class="img-text">我的分销团队</div>
       <div class="img-box">
@@ -88,6 +88,7 @@
     font-size: $font-size-14
     color: #464646
     font-family: $font-family-regular
+    padding: 0 15px
     margin-top: 24px
   .img-box
     margin-top: 12.5px
