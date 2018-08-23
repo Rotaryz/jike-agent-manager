62d6a99e5ad7ac28a00ee66450fda28761316702
guojing <939316942@qq.com>
2018-08-23 14:15:30 +0800

    切换微店、名片文字

--- src/pages/custom-detail/custom-detail.vue
+++ src/pages/custom-detail/custom-detail.vue
@@ -25,7 +25,7 @@
         </div>
       </div>
       <div class="list-sec">
-        <h3 class="title"><span class="name">AI微店购买记录</span></h3>
+        <h3 class="title"><span class="name">AI{{isWD?'微店':'名片'}}购买记录</span></h3>
         <div class="tab">
           <p class="custom">日期</p>
           <p class="count">购买个数/个</p>
@@ -63,6 +63,8 @@
   import { ERR_OK } from 'common/js/config'
   import Toast from 'components/toast/toast'
   import EmptyData from 'components/empty-data/empty-data'
+  import storage from 'storage-controller'
+  import { WEI_SHANG, PROJECT_ARR } from 'common/js/constant'
 
   export default {
     name: 'custom-detail',
@@ -81,7 +83,8 @@
         id: null,
         agentId: '',
         msg: '',
-        showEmpty: false
+        showEmpty: false,
+        isWD: true
       }
     },
     created() {
@@ -102,6 +105,12 @@
       }
     },
     methods: {
+      _getProject() {
+        const project = storage.get('project')
+        const obj = PROJECT_ARR.find(val => val.project === project)
+        this.form.title = obj.name
+        this.isWD = project === WEI_SHANG.project
+      },
       _checkEmpty() {
         this.showEmpty = !this.dataArray.length
       },
